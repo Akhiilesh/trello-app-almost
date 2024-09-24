3650d822660d682537b4d100f9d9cc5288865ab4
Akhilesh Sanap <akhileshsanap091@gmail.com>
2024-09-24 22:43:46 +0530

    Style Components Add

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,3 @@
-import { motion } from "framer-motion";
-
-export default function App() {
-  return (
-    <>
-      <h1>SomethingHere</h1>
-      <motion.button className="bg-white">Button</motion.button>
-    </>
-  );
-}
+export const App = () => {
+  return null;
+};
